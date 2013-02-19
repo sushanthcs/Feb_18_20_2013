b04891f877c1fe13aa15515723c3cc600d9d2de9
Prabhu Sunderaraman <iamprabhu@yahoo.com>
2013-02-19 10:35:48 +0530

    Lab 02

---
+++ Day02/src/com/lab02/DSL1.groovy
@@ -0,0 +1,15 @@
+package com.lab02
+
+Pizza {
+	type large
+	quantity 3
+	toppings "mushroom,onions"
+	address {
+		street "InfoPark"
+		city "Kochi"
+		state "Kerala"
+	}
+}
+//Thanks for the Order!!!Your large sized Pizza(3 nos) with 
+//mushroom,onion toppings will be delivered to InfoPark, Kochi, Kerala in
+//45 minutes
