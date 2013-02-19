12a757ab212db3f552fff9ebe6f0fd54c623cb30
Prabhu Sunderaraman <iamprabhu@yahoo.com>
2013-02-19 09:49:31 +0530

    Day 02

---
+++ Day02/src/Person.groovy
@@ -0,0 +1,13 @@
+class Person {
+	String name
+	int age
+	def propertyMissing(String propName,args){
+		println "*************"
+		println "Property ${propName} is not found"
+		println args
+		println "**************"
+	}
+	def methodMissing(String methodName,args){
+		println "Method ${methodName} not found"
+	}
+}
---
+++ Day02/src/PersonUser.groovy
@@ -0,0 +1,5 @@
+p1 = new Person(name:"Sam",age:12)
+p1.eat()
+p1.dumdskgg()
+p1.gender = "Male"
+println p1.salary
