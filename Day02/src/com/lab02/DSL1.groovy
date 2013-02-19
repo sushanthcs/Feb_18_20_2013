0ec0b05ed0683e40249304682abe48f5fe74c513
Prabhu Sunderaraman <iamprabhu@yahoo.com>
2013-02-19 10:52:37 +0530

    Lab 02

--- Day02/src/com/lab02/DSL1.groovy
+++ Day02/src/com/lab02/DSL1.groovy
@@ -1,7 +1,39 @@
 package com.lab02
+order = [:]
+
+def Pizza(block){
+	block()
+	def output = """
+Thanks for the order!!! Your ${order["type"]} sized Pizza (${order["quantity"]} nos) 
+with ${order["toppings"]} toppings will be delivered to ${order["street"]},${order["city"]},
+${order["state"]} in 45 minutes
+"""
+	println output
+}
+def type(val){
+	order["type"] = val
+}
+def quantity(val){
+	order["quantity"] = val
+}
+def toppings(val){
+	order["toppings"] = val
+}
+def address(block){
+	block()
+}
+def street(val){
+	order["street"] = val
+}
+def city(val){
+	order["city"] = val
+}
+def state(val){
+	order["state"] = val
+}
 
 Pizza {
-	type large
+	type "large"
 	quantity 3
 	toppings "mushroom,onions"
 	address {
